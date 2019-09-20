4a7793c6d010bdc6de37345ff17d65d973f29f6c
NewtonXu <newton.xu@hotmail.com>
2019-09-20 17:39:25 -0400

    Added custom set option

--- SiXuCapitalOne.py
+++ SiXuCapitalOne.py
@@ -1,4 +1,6 @@
 '''
+Solution Overview
+========================
 For this assignment, I was only familiar with the two commenting styles:
 Python #, and C-style /* */, //. 
 
@@ -7,12 +9,36 @@ sufficient to cover the majority of popular languages. That is, one case where
 the single line comment and multiline comments are identical, and one case
 where the multiline comment has different symbols.
 
-The main edge case I encountered was comment symbols inside strings. 
+The CStyleFSM is capable of handling any programming language with both single
+line and multi-line comments. The symbol set of //, /* */ seems to apply quite
+broadly. 
+
+CStyleFSM can also handle languages with only multi-line 
+comments like HTML. In this case I made the assumption that all comments here 
+should be treated as multiline comments as given in the example.
+
+The PythonFSM will handle cases where there are only single line comments.
+
+Edge Cases
+========================
+Case 1)
+// /* */
+In this situation, there is a single line comment and a multiline comment on 
+the same line. My system counts the multi-line first, this is for compatibility
+with languages like HTML which do not have single line comments. 
+
+Case 2)
+A string containing a comment symbol on the same line as a comment
+string = "#" #"commentnotstr
+
+
+
 
 '''
 
-class commentCounter:
+class CommentCounter:
     def __init__(self): 
+        self.FSMType = True
         self.PythonFSM = 0
         self.CStyleFSM = 0
         self.lineCount = 0
@@ -24,6 +50,7 @@ class commentCounter:
         self.singleLineComment = ""
         self.multiLineCommentStart = ""
         self.multiLineCommentEnd = ""
+        self.TODO = "TODO"
 
     def reset(self):
         self.PythonFSM = 0
@@ -34,17 +61,39 @@ class commentCounter:
         self.multilineCount = 0
         self.linesInMultiline = 0
         self.numToDo = 0
-        self.singleLineComment = ""
-        self.multiLineCommentStart = ""
-        self.multiLineCommentEnd = ""
-    
+
+    def useMultilineFSM(self,input):
+        '''
+        Tell us if we should use the multiline FSM or singleline FSM
+        '''
+        self.FSMType = input
+        
     def loadSingleLineComment(self, input):
+        '''
+        String which represents the start of a single line comment
+        '''
         self.singleLineComment = input
     
     def loadMultiLineComment(self, start_symbol, end_symbol):
+        '''
+        String which represents the start/end of a multi line comment
+        '''
         self.multiLineCommentStart = start_symbol
         self.multiLineCommentEnd = end_symbol
-       
+        
+    def loadCustomSymbols(self, singleLineSymbol, multiLineStart, multiLineEnd):
+        '''
+        Load a set of symbols for your favourite programming language
+        '''
+        self.loadSingleLineComment(singleLineSymbol)
+        self.loadMultiLineComment(multiLineStart, multiLineEnd)
+    
+    def loadTODO(self, input):
+        '''
+        Load a different string to represent the TODO
+        '''
+        self.TODO = input
+    
     def loadPythonCommentPreset(self):
         '''
         Sample comment symbols for Python
@@ -85,14 +134,14 @@ class commentCounter:
     def checkPythonComment(self, line):
         '''
         Use this to compute comments where the single line comment and multiline
-        comments use identical identifiers, ie Python, HTML
+        comments use identical identifiers, ie Python
         '''
         if self.PythonFSM == 0:
             #Base Case
-            if "#" not in line:
+            if self.singleLineComment not in line:
                 return
             else:
-                if line[0]=="#":
+                if line[0]==self.singleLineComment:
                     self.PythonFSM = 1
                     self.checkPythonComment(line)
                 else:
@@ -103,30 +152,30 @@ class commentCounter:
             #Single line only
             self.singleLineCount += 1
             self.commentCount += 1
-            if "TODO" in line:
+            if self.TODO in line:
                 self.numToDo += 1
             self.PythonFSM = 0
         
         elif self.PythonFSM == 1:
-            # If line[0] is hash
+            # If line[0] is comment symbol
             self.singleLineCount += 1
             self.commentCount += 1
-            if "TODO" in line:
+            if self.TODO in line:
                 self.numToDo += 1
             self.PythonFSM = 2
         
         elif self.PythonFSM == 2:
-            #Waiting to see if single line becomes multiline
-            if "#" not in line:
+            #Waiting to see if single line comment becomes multiline
+            if self.singleLineComment not in line:
                 self.PythonFSM = 0
             else:
-                if line[0]=="#":
+                if line[0]==self.singleLineComment:
                     self.PythonFSM = 3
                     self.checkPythonComment(line)
                 else:
                     self.PythonFSM = 5
                     self.CheckPythonComment(line)
-                if "TODO" in line:
+                if self.TODO in line:
                     self.numToDo += 1
         
         elif self.PythonFSM == 3:
@@ -136,16 +185,16 @@ class commentCounter:
             self.multilineCount += 1
             self.linesInMultiline += 2
             self.PythonFSM = 4
-            if "TODO" in line:
+            if self.TODO in line:
                 self.numToDo += 1
         
         elif self.PythonFSM == 4:
             #Inside a multiline, waiting to end
-            if "#" in line:
-                if line[0]=="#":
+            if self.singleLineComment in line:
+                if line[0]==self.multiLineCommentStart:
                     self.commentCount+=1
                     self.linesInMultiline += 1
-                    if "TODO" in line:
+                    if self.TODO in line:
                         self.numToDo += 1
                 else:
                     self.PythonFSM = 5
@@ -159,25 +208,25 @@ class commentCounter:
         comments are different EG: C++, Java 
         '''
         if self.CStyleFSM==0:
-            if "/*" in line:
+            if self.multiLineCommentStart in line:
                 self.commentCount += 1
                 self.multilineCount += 1
                 self.linesInMultiline += 1
-                if "TODO" in line:
+                if self.TODO in line:
                     self.numToDo += 1
-                if "*/" not in line:
+                if self.multiLineCommentEnd not in line:
                     self.CStyleFSM = 1
                     
-            elif "//" in line:
+            elif self.singleLineComment in line:
                 self.commentCount += 1
                 self.singleLineCount += 1
-                if "TODO" in line:
+                if self.TODO in line:
                     self.numToDo += 1
             
         elif self.CStyleFSM==1:
-            if "TODO" in line:
+            if self.TODO in line:
                 self.numToDo += 1
-            if "*/" in line:
+            if self.multiLineCommentEnd in line:
                 self.commentCount += 1
                 self.linesInMultiline += 1
                 self.CStyleFSM = 0
@@ -185,38 +234,42 @@ class commentCounter:
                 self.commentCount += 1
                 self.linesInMultiline += 1
 
-    def runPythonFSM(self,filename):
+    def runFSM(self,filename):
+        '''
+        Main function for file parsing
+        '''
         with open(filename, "r") as file:
             for line in file:
                 line.strip()
-                if(line!="\n"):
-                    self.lineCount += 1
-                self.checkPythonComment(line)
+                self.lineCount += 1
+                if self.FSMType == False:
+                    self.checkPythonComment(line)
+                else:
+                    self.checkOtherComment(line)
         self.printAll()
     
-    def runCStyleFSM(self, filename):
-        with open(filename, "r") as file:
-            for line in file:
-                line.strip()
-                if(line!="\n"):
-                    self.lineCount += 1
-                self.checkOtherComment(line)
-        self.printAll()
-        
     def checkFile(self, filename):
+        '''
+        Entry point into the algorithm
+        '''
         self.reset()
         if filename[0] == ".":
+            print("This file starts with . ?")
             return
-        name, extension = filename.split(".")
-        if extension == "py":
-            self.runPythonFSM(filename)
-        else:
-            self.runCStyleFSM(filename)
+        filename_info = filename.split(".")
+        if len(filename_info)<2:
+            print("This file has no extension ?")
+            return
+        self.runFSM(filename)
         
         
         
 if __name__ == "__main__":
-    javaChecker = commentCounter()
-    javaChecker.reset()
+    javaChecker = CommentCounter() #initialize the class
+    javaChecker.loadCPPCommentPreset() #load the symbols for this language
+    #I've also included two other options:
+    #javaChecker.loadPythonCommentPreset()
+    #javaChecker.loadCustomSymbols(str,str,str)
+    javaChecker.useMultilineFSM(True) 
     filename = "test.txt"
     javaChecker.checkFile(filename)
--- test.py
+++ test.py
@@ -8,11 +8,10 @@
 #Author: Ali Bhagat
 #This script helps to build a simple stopwatch application using Python's time module.
 import time
-print('Press ENTER to begin, Press Ctrl + C to stop')
+print('Press ENTER to begin, Press Ctrl + C to stop#')
 while True:
 try:
-input() # For ENTER. Use raw_input() if you are running python 2.x instead of
-input()
+input() # For ENTER. Use raw_input() if you are running python 2.x instead of input()
 starttime = time.time()
 print('Started')
 except KeyboardInterrupt:
